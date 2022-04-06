98700f4146e125335ec18c54b1d610f05c408559
James Draper <jd.unchecked@gmail.com>
2022-04-06 11:35:29 +0100

    Add get tests.

--- test/StoreTest.php
+++ test/StoreTest.php
@@ -9,4 +9,39 @@ use PHPUnit\Framework\TestCase;
 
 class StoreTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function get_returns_null_if_key_does_not_exist(): void
+    {
+        $store = new Store(__DIR__ . '/test_get.sqlite');
+
+        $result = $store->get('KEY 4');
+
+        $this->assertNull($result);
+    }
+
+    /**
+     * @test
+     */
+    public function get_returns_default_if_key_does_not_exist(): void
+    {
+        $store = new Store(__DIR__ . '/test_get.sqlite');
+
+        $result = $store->get('KEY 4', 'VALUE 4');
+
+        $this->assertSame('VALUE 4', $result);
+    }
+
+    /**
+     * @test
+     */
+    public function get_returns_value(): void
+    {
+        $store = new Store(__DIR__ . '/test_get.sqlite');
+
+        $result = $store->get('KEY 1');
+
+        $this->assertSame('VALUE 1', $result);
+    }
 }
