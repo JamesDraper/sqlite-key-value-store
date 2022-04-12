c53baa38467fda9e22924459d448346dc248b82b
James Draper <jd.unchecked@gmail.com>
2022-04-12 21:17:52 +0100

    Add search value test.

---
+++ test/SearchValueTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Test;
+
+class SearchValueTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_search_values(): void
+    {
+        $result = $this->store->searchValue('*3*');
+
+        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_search_values_with_non_standard_wildcards(): void
+    {
+        $result = $this->store->searchValue('_3_', '_');
+
+        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_search_values_with_percentage_sign_wildcards(): void
+    {
+        $result = $this->store->searchValue('%3%', '%');
+
+        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
+    }
+}
