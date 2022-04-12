00e78ea3477309d26a4c97006795428491221c14
James Draper <jd.unchecked@gmail.com>
2022-04-12 21:16:17 +0100

    Add search key tests.

---
+++ test/SearchKeyTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Test;
+
+class SearchKeyTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_search_keys(): void
+    {
+        $result = $this->store->searchKey('*3*');
+
+        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_search_keys_with_non_standard_wildcards(): void
+    {
+        $result = $this->store->searchKey('_3_', '_');
+
+        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
+    }
+
+    /**
+     * @test
+     */
+    public function it_should_search_keys_with_percentage_sign_wildcards(): void
+    {
+        $result = $this->store->searchKey('%3%', '%');
+
+        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
+    }
+}
--- test/StoreTest.php
+++ test/StoreTest.php
@@ -52,36 +52,6 @@ class StoreTest extends TestCase
     /**
      * @test
      */
-    public function it_should_search_keys(): void
-    {
-        $result = $this->store->searchKey('*3*');
-
-        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
-    }
-
-    /**
-     * @test
-     */
-    public function it_should_search_keys_with_non_standard_wildcards(): void
-    {
-        $result = $this->store->searchKey('_3_', '_');
-
-        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
-    }
-
-    /**
-     * @test
-     */
-    public function it_should_search_keys_with_percentage_sign_wildcards(): void
-    {
-        $result = $this->store->searchKey('%3%', '%');
-
-        $this->assertSame(['KEY 3' => 'VALUE 3'], $result);
-    }
-
-    /**
-     * @test
-     */
     public function it_should_search_values(): void
     {
         $result = $this->store->searchValue('*3*');
